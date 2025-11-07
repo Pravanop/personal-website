b7e1cbafb82d1b630d765add66c70976b6635637
Pravan Omprakash <pravanomprakash.181mt035@nitk.edu.in>
2025-11-07 10:51:39 -0600

    fixing broken css

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,13 @@ eleventyConfig.addNunjucksGlobal("now", new Date());
   eleventyConfig.addNunjucksFilter("date", function(dateObj, format = "MMMM d, yyyy") {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
   });
+  eleventyConfig.setBrowserSyncConfig({
+      files: './_site/**/*',
+    });
 
+    module.exports = {
+      pathPrefix: "/personal-website/",
+    };
   // Define the "posts" collection
   eleventyConfig.addCollection("posts", function(collection) {
     return collection.getFilteredByGlob("./posts/*.md");
